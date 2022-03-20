2de7e341cf7b74cbd0c6d21fe3aff714dd860c05
GabrielViola95 <92824009+GabrielViola95@users.noreply.github.com>
2022-03-20 18:16:56 -0300

    final commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,9 +22,14 @@ const Home = () => {
       <div className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
         <h1 className='text-xl mb-4'>Home</h1>
         <p>Hola {user.displayName || user.email} ¿Qué quieres hacer hoy?</p>
+        <div className="m-3">
+        {user.photoURL ? <img className='m-auto rounded shadow-sm' src={user.photoURL} alt="profile-img" /> : 
+        <img src="https://0.soompi.io/wp-content/uploads/sites/8/2018/10/08125502/mistery-guy.jpg?s=900x600&e=t" alt="usuario-anonimo" />}
+
+        </div>
 
         <button onClick={handleLogOut} className='bg-slate-200 hover:bg-slate-300 rounded py-2 px-4'>Desconectarse</button>
-
+      
       </div>
     </div>
   )

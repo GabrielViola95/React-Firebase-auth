3e56ce1ad71ef7d8564137c42510d4031667773f
GabrielViola95 <92824009+GabrielViola95@users.noreply.github.com>
2022-03-20 03:37:23 -0300

    create components and others

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>} />
+    </Routes>
   );
 }
 
---
+++ src/components/ProtecterRouter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProtecterRouter = () => {
+  return (
+    <div>
+        <h1>ProtecterRouter</h1>
+    </div>
+  )
+}
+
+export default ProtecterRouter
